de4f36475c5458a8f5a3138ca9e9a47ed0738173
Fergus Greig <fergus.greig@gmail.com>
2020-07-30 17:06:16 +0100

    added make grid function

--- designs.js
+++ designs.js
@@ -3,8 +3,14 @@
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
-
-// Your code goes here!
-
+function makeGrid(rows,cols) {
+    var row = newFunction();
+    var table = document.getElementById(pixelCanvas);
+    table.innerHTML = ''; // clears the grid
+    for (var i = 0; i<rows; i++){
+        row = table.insertRow(i);
+        for (var j = 0; j<cols; j++){
+            row.insertCell(j)
+        }
+    }
 }
