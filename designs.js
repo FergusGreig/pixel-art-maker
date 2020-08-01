6c5f3af8182f44ac79984b76623beddb212be568
Fergus Greig <fergus.greig@gmail.com>
2020-08-01 15:04:13 +0100

    working art maker

--- designs.js
+++ designs.js
@@ -1,10 +1,6 @@
-// Select color input
-// Select size input
 
-// When size is submitted by the user, call makeGrid()
-
+// FUNCTIONS
 function makeGrid(rows,cols) {
-    const table = document.getElementById('pixelCanvas');
     table.innerHTML = ''; // clears the grid
     for (var i = 0; i<rows; i++){
         row = table.insertRow(i);
@@ -13,13 +9,22 @@ function makeGrid(rows,cols) {
         }
     }
 }
+
+//Delclarations
 var width;
 var height;
-
+const table = document.getElementById('pixelCanvas');
+const picker = document.getElementById('colourPicker');
 const form = document.getElementById('sizePicker');
+
+// Events 
 form.addEventListener("submit",function(event){
     event.preventDefault();
     height = parseInt(form.elements[0].value, 10);
     width = parseInt(form.elements[1].value,10);
     makeGrid(height,width)
+})
+
+table.addEventListener('click',function(event){
+    event.target.style.backgroundColor = picker.value
 })
